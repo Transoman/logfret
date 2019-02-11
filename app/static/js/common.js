089fd8f76f51921db32b0958992719c9d7bc30dd
transoman <vasiag10@gmail.com>
2019-02-11 18:06:32 +0200

    Layout article section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -20,7 +20,25 @@ jQuery(document).ready(function($) {
       el: '.swiper-pagination',
       clickable: true
     }
-  })
+  });
+
+  var fullwidth = function() {
+    var el = $('.article__img-wrap');
+    var position = el.position();
+    var left = position.left;
+    console.log(position);
+    var right = $(window).width() - position.left - el.width();
+    if (left != 0) {
+      el.css({'left': '-' + left + 'px', 'right': '-' + right + 'px', 'width': $(window).width() + 'px'});
+    }
+  }
+
+  fullwidth();
+
+  $(window).resize(function() {
+    // fullwidth();
+  });
+
 
   // SVG
   svg4everybody({});
